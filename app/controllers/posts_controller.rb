247f9dfca6c18daaab4d3c2d091b25933c06f661
Dean Gibbons <dgibbons89@gmail.com>
2015-03-09 22:16:54 -0400

    fixed sql

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     if params[:category].blank?
       @posts = Post.all.order("created_at DESC")
     else
-      @category_id = Post.find_by(name: params[:category])
+       @category_id = Category.find_by(name: params[:category]).id
       @posts = Post.where(category_id: @category_id).order("created_at DESC")
     end
   end
